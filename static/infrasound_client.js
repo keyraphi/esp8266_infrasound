d84287dc952c842f74a21675fda7ff0514b12287
Raphael Braun <keyraphi@gmail.com>
2024-06-28 22:06:09 +0200

    Started on wifi setting page. Submit button is not working yet

--- static/infrasound_client.js
+++ static/infrasound_client.js
@@ -1,68 +1,79 @@
 pfft_module = null;
 pffft().then(async function(Module) {
-        if(true) console.log("PFFFT Module initialized");
-        pffft_module = Module;
-    });
-
+  if (true) console.log("PFFFT Module initialized");
+  pffft_module = Module;
+});
+var chartTimeSeriesDuration = 30 * 50;
 var chartTimeSeries = new Highcharts.Chart({
-  chart:{
+  chart: {
     renderTo: "infrasound-time-serie",
-      animation: false
+    animation: false
   },
-  title:{text: "Zeit Serie"},
-  series:[{
+  title: { text: "Zeit Serie" },
+  series: [{
     showInLegend: false,
     data: []
   }],
-  plotOptions:{
+  plotOptions: {
     line: {
-      dataLabels: {enabled: false},
+      dataLabels: { enabled: false },
     },
-    series:{color: '#059e8a', 
+    series: {
+      color: '#059e8a',
     },
   },
-  xAxis: {type: 'datetime',
-    dateTimeLabelFormats: {second: "%H:%M:%S"}
+  xAxis: {
+    type: 'datetime',
+    dateTimeLabelFormats: { second: "%H:%M:%S" }
   },
   yAxis: {
-    title: {text: "Relativer Luftdruck [Pa]"}
+    title: { text: "Relativer Luftdruck [Pa]" }
   },
-  credits: {enabled: false},
+  credits: { enabled: false },
   time: {
     timezone: "Europe/Berlin",
   }
 });
 
 var chartSpectrum = new Highcharts.Chart({
-  chart:{
+  chart: {
     type: "area",
     renderTo: "infrasound-spectrum",
-      animation: false
+    animation: false
   },
-  title:{text: "Spektrum"},
-  series:[{
+  title: { text: "Spektrum" },
+  series: [{
     showInLegend: false,
     data: []
   }],
-  plotOptions:{
+  plotOptions: {
     line: {
-      dataLabels: {enabled: false},
+      dataLabels: { enabled: false },
     },
-    series:{color: '#059e8a', 
+    series: {
+      color: '#059e8a',
     },
   },
-  xAxis: {type: 'linear', title: {text: "Frequenz [Hz]"}},
+  xAxis: { type: 'linear', title: { text: "Frequenz [Hz]" } },
   yAxis: {
-    title: {text: "Amplitude [Pa]"}
+    title: { text: "Amplitude [Pa]" }
   },
-  credits: {enabled: false}
+  credits: { enabled: false }
 });
 
 
 var movingSequence = [];
+var movingChartData = [];
 var pffft_runner = null;
 var dataPtr = null;
 var dataHeap = null;
+
+function cleanup_pfft() {
+  pffft_module._free(dataPtr);
+  pffft_module._pffft_runner_destroy(pffft_runner);
+  dataPtr = null;
+  pffft_runner = null;
+}
 
 function initialize_pfft(fft_window) {
   if (pffft_runner) {
@@ -72,7 +83,7 @@ function initialize_pfft(fft_window) {
   var bytes_per_element = 4;
   var nDataBytes = audio_block_size * bytes_per_element;
 
-  pffft_runner = pffft_module._pffft_runner_new(audio_block_size,bytes_per_element);
+  pffft_runner = pffft_module._pffft_runner_new(audio_block_size, bytes_per_element);
 
   dataPtr = pffft_module._malloc(nDataBytes);
   dataHeap = new Uint8Array(pffft_module.HEAPU8.buffer, dataPtr, nDataBytes);
@@ -96,26 +107,31 @@ function fourier_transform(timeSequence) {
 function updateCharts(dataJson) {
   timeArrayMS = dataJson["ms"];
   preassurePA = dataJson["preassure"];
-  var fft_window = 1024
+  var fft_window = 2 ** document.getElementById("spectrumRange").value;
 
   for (let i = 0; i < timeArrayMS.length; i++) {
     var x = timeArrayMS[i];
     var y = preassurePA[i];
     movingSequence.push(y);
-    
-    if(chartTimeSeries.series[0].data.length > 30 * 60) {
+    movingChartData.push([x, y]);
+
+    if (chartTimeSeries.series[0].data.length == chartTimeSeriesDuration) {
       chartTimeSeries.series[0].addPoint([x, y], false, true, false);
+    } else if (chartTimeSeries.series[0].data.length > chartTimeSeriesDuration) {
+      console.log("Setting chart data to", movingChartData.slice(-chartTimeSeriesDuration));
+      chartTimeSeries.series[0].setData(movingChartData.slice(-chartTimeSeriesDuration));
     } else {
       chartTimeSeries.series[0].addPoint([x, y], false, false, false);
     }
     chartTimeSeries.update({}, true, false, false);
   }
-  
-  movingSequence = movingSequence.slice(-fft_window);
-  var fft_time_sequence = Array.from(movingSequence)
+
+  movingSequence = movingSequence.slice(-4096);
+  var fft_time_sequence = Array.from(movingSequence.slice(-fft_window));
   if (fft_time_sequence.length < fft_window) {
-    var padding = new Array(fft_window - fft_time_sequence.length).fill(0)
-    fft_time_sequence = fft_time_sequence.concat(padding)
+    // pad up with zeros
+    var padding = Array(fft_window - fft_time_sequence.length).fill(0);
+    fft_time_sequence = fft_time_sequence.concat(padding);
   }
 
   if (!pffft_module) {
@@ -133,7 +149,7 @@ function updateCharts(dataJson) {
   var test = [];
   for (var i = 0; i < spectrum.length; i++) {
     var hz = i * 50.0 / fft_time_sequence.length;
-    test[i] = [hz, spectrum[i]**0.5 * 2 / fft_time_sequence.length];
+    test[i] = [hz, spectrum[i] ** 0.5 * 2 / fft_time_sequence.length];
   }
   chartSpectrum.series[0].setData(test);
   chartSpectrum.update({}, true, false, false);
@@ -145,10 +161,27 @@ setInterval(function() {
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        var measurements = JSON.parse(this.responseText);
-        updateCharts(measurements);
+      var measurements = JSON.parse(this.responseText);
+      updateCharts(measurements);
     }
   };
   xmlhttp.open("GET", "/measurements", true);
   xmlhttp.send();
 }, 200);
+
+document.getElementById("timeSeriesRange").value = chartTimeSeriesDuration;
+document.getElementById("timeSeriesRangeIndicator").innerHTML = "Dargestellte Zeitspanne: " + chartTimeSeriesDuration / 50 + " seconds";
+// Slider for time duration 
+document.getElementById("timeSeriesRange").oninput = function() {
+  var val = document.getElementById("timeSeriesRange").value;
+  chartTimeSeriesDuration = val;
+  document.getElementById("timeSeriesRangeIndicator").innerHTML = "Dargestellte Zeitspanne: " + val / 50 + " seconds";
+};
+
+document.getElementById("SpectrumRangeIndicator").innerHTML = "Spektrum Analyse Dauer: " + 2 ** document.getElementById("spectrumRange").value / 50.0 + " seconds (" + 2 ** document.getElementById("spectrumRange").value + " samples)";
+document.getElementById("spectrumRange").oninput = function() {
+  var val = 2 ** document.getElementById("spectrumRange").value;
+  cleanup_pfft();
+  initialize_pfft(val);
+  document.getElementById("SpectrumRangeIndicator").innerHTML = "Spektrum Analyse Dauer: " + val / 50.0 + " seconds (" + val + " samples)";
+};
--- tests/server.py
+++ tests/server.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 import json
+from flask import Flask, request, send_from_directory
 
-from http.server import SimpleHTTPRequestHandler, HTTPServer
 import time
 import os
 import numpy as np
 
 static_directory = os.path.abspath("../static")
+
+app = Flask(
+    __name__,
+    static_url_path="",
+    static_folder=static_directory,
+)
+
 
 def tone(samples, frequency):
     amplitude = 5
@@ -15,38 +22,48 @@ def tone(samples, frequency):
     buffer += np.random.normal(0, 10, len(samples))
     return buffer
 
-class WebRequestHandler(SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == "/measurements":
-            now = int(time.time()*1000)
-            times = np.array([now + 20 * i for i in range(10)])
-            response = json.dumps(
-                {
-                    "ms": times.tolist() ,
-                    "preassure": tone(times/1000, 0.8).tolist()
-                }
-            )
-            self.send_response(200)
-            self.send_header("Content-Type", "application/json")
-            self.end_headers()
-            self.wfile.write(response.encode("utf-8"))
-        elif self.path == "/downloads":
-            response = json.dumps(
-                {
-                "download-links": ["/test/file_example_WAV_1MG.wav", "/test/file_example_WAV_2MG.wav"],
-                "link-texts": ["file_example_WAV_1MG.wav","file_example_WAV_2MG.wav"]
-                }
-            )
-            print(response)
-            self.send_response(200)
-            self.send_header("Content-Type", "application/json")
-            self.end_headers()
-            self.wfile.write(response.encode("utf-8"))
-        else:
-            super().do_GET()
+@app.route("/")
+def index_handler():
+    return send_from_directory(static_directory, "index.html")
+
+@app.route("/measurements", methods=["GET"])
+def handle_measurement_request():
+    now = int(time.time() * 1000)
+    times = np.array([now + 20 * i for i in range(10)])
+    response = json.dumps(
+        {"ms": times.tolist(), "preassure": tone(times / 1000, 0.8).tolist()}
+    )
+    return response
+
+
+@app.route("/downloads", methods=["GET"])
+def handle_downloads_request():
+    response = json.dumps(
+        {
+            "download-links": [
+                "/test/file_example_WAV_1MG.wav",
+                "/test/file_example_WAV_2MG.wav",
+            ],
+            "link-texts": ["file_example_WAV_1MG.wav", "file_example_WAV_2MG.wav"],
+        }
+    )
+    return response
+
+
+@app.route("/set_wifi", methods=["POST"])
+def handle_wifi_form():
+    # Access form data using request.form dictionary
+    print(request.form)
+    ssid = request.form.get("ssid")
+    password = request.form.get("password")
+    print("SSID:", ssid, "password", password)
+    # TODO fix this!
+
+    # Process the extracted data (SSID and password) here
+    # ... your logic to handle SSID and password ...
+
+    return f"I received ssid={ssid} and password={password}"
 
 
 if __name__ == "__main__":
-    print(static_directory)
-    server = HTTPServer(("localhost", 8000), lambda *_: WebRequestHandler(*_, directory=static_directory))
-    server.serve_forever()
+    app.run(debug=True, port=8000)
